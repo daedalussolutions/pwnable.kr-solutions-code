15b982cc4921736b0f077adf3082b66331a417bc
daedalussolutions <dylancassell000@hotmail.com>
2024-03-12 17:57:55 -0400

    Initial commit.

---
+++ random/random.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+        unsigned int random;
+        random = rand();        // random value!
+
+        printf("random is %x\n", random);
+        return 0;
+}
